286592d02726990d09a141b6ce7d4e7158c633a5
alexander.kravchuk <alexander.kravchuk@nakitel.com>
2018-02-02 12:50:28 +0200

    add todo function in reducer todos

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,23 +1,38 @@
+function todo(state, action) {
+
+   switch (action.type) {
+		 case 'ADD_TODO' : {
+ 			return{
+ 					id : action.id,
+ 					text : action.text,
+ 					complated : false
+ 				}
+ 		}
+		case 'TOGGLE_TODO': {
+         if(state.id !== action.id){
+					 return state
+				 }
+				 return {
+					 ...state,
+					 complated : !state.complated
+				 }
+		}
+   	default:
+   		return state
+   }
+
+}
+
 export default function todos(state = [], action){
 	switch (action.type){
 		case 'ADD_TODO' : {
 			return [
-				{
-					id : action.id,
-					text : action.text,
-					complated : false
-				},
+				todo(state, action),
 				...state
 			]
 		}
 		case 'TOGGLE_TODO': {
-			return state.map( item =>
-				 item.id === action.id
-				 ? {
-					 ...item,
-					 complated : !item.complated
-				 } : item
-			)
+			return state.map( item => todo(item, action))
 		}
 		default : {
       return state
